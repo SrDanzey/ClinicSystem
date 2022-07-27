151dd3f64665883f1cbec16df53909613fd2a947
SrDanzey <douglasguimaaraes@gmail.com>
2022-07-27 18:09:40 -0300

    Clinica - Cria CRUD do Clinica

--- src/br/com/clinicsystem/agendaconsultoria/core/clinica/ClinicaService.java
+++ src/br/com/clinicsystem/agendaconsultoria/core/clinica/ClinicaService.java
@@ -1,4 +1,33 @@
 package br.com.clinicsystem.agendaconsultoria.core.clinica;
 
+import br.com.clinicsystem.agendaconsultoria.core.validacao.exception.NegocioException;
+
+import java.util.ArrayList;
+
 public class ClinicaService {
+
+    public String salvarClinica(ClinicaEntity clinica) throws NegocioException {
+        ClinicaBO clinicaBO = new ClinicaBO();
+        return clinicaBO.salvarClinica(clinica);
+    }
+
+    public ArrayList<ClinicaEntity> listarClinica() throws NegocioException {
+        ClinicaBO clinicaBO = new ClinicaBO();
+        return clinicaBO.listarClinica();
+    }
+
+    public void deletarClinica(Long idClinica) throws NegocioException {
+        ClinicaBO clinicaBO = new ClinicaBO();
+        clinicaBO.deletarClinica(idClinica);
+    }
+
+    public ClinicaEntity buscarClinica(Long idClinica) throws NegocioException {
+        ClinicaBO clinicaBO = new ClinicaBO();
+        return clinicaBO.buscarClinica(idClinica);
+    }
+
+    public String alterarClinica(ClinicaEntity clinica) throws NegocioException {
+        ClinicaBO clinicaBO = new ClinicaBO();
+        return clinicaBO.alterarClinica(clinica);
+    }
 }
